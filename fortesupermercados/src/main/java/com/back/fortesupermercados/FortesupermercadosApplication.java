3abb6239ed882812e48a694cfd9c7fb9aae96c25
Cesar Glufke Zanellato <cesar.g.zanellato@gmail.com>
2024-04-04 21:53:56 -0300

    Criação do Back do ForteSupermercado

---
+++ fortesupermercados/src/main/java/com/back/fortesupermercados/FortesupermercadosApplication.java
@@ -0,0 +1,13 @@
+package com.back.fortesupermercados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortesupermercadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FortesupermercadosApplication.class, args);
+	}
+
+}
