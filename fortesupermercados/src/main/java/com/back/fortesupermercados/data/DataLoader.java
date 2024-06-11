154be485ea07cf69ac1aca241600622ed09b8554
Cesar Glufke Zanellato <cesar.g.zanellato@gmail.com>
2024-06-10 21:59:27 -0300

    create assets

--- fortesupermercados/src/main/java/com/back/fortesupermercados/data/DataLoader.java
+++ fortesupermercados/src/main/java/com/back/fortesupermercados/data/DataLoader.java
@@ -54,14 +54,14 @@ public class DataLoader implements ApplicationRunner{
     }
 
     List<String> categories = List.of(
-        "ALIMENTOSBASICOS", "BEBIDAS", "MATINAIS", "HIGIENEPESSOAL", "LIMPEZA", "PADARIA", "HORTIFRUIT", "ACOUGUE"
+        "ALIMENTOS_BASICOS", "BEBIDAS", "MATINAIS", "HIGIENE_PESSOAL", "LIMPEZA", "PADARIA", "HORTIFRUIT", "ACOUGUE"
     );
 
     Map<String, List<String>> subcategoriesMap = new HashMap<>(Map.of(
-        "ALIMENTOSBASICOS", List.of("ACUCAR", "ARROZ", "AZEITE", "FARINHA", "FAROFA", "FEIJAO", "GRAOS", "MASSAS", "OLEO", "OVO", "SAL", "VINAGRE", "OUTROS_ALIMENTOS_BASICOS"),
+        "ALIMENTOS_BASICOS", List.of("ACUCAR", "ARROZ", "AZEITE", "FARINHA", "FAROFA", "FEIJAO", "GRAOS", "MASSAS", "OLEO", "OVO", "SAL", "VINAGRE", "OUTROS_ALIMENTOS_BASICOS"),
         "BEBIDAS", List.of("AGUA_COM_GAS", "AGUA_SEM_GAS", "AGUA_TONICA", "BEBIDAS_DIVERSAS", "ENEGETICO_GARRAFA", "ENEGETICO_LATA","REFRIGERANTE_DOIS_LITROSORIGINAL", "REFRIGERANTE_DOIS_LITROSZERO", "REFRIGERANTE_MEIO_LITROORIGINAL", "REFRIGERANTE_MEIO_LITROZERO", "REFRIGERANTE_LATA_ORIGINAL", "REFRIGERANTE_LATA_ZERO", "REFRIGERANTE_PEQUENO_ORIGINAL", "REFRIGERANTE_PEQUENO_ZERO","REFRIGERANTE_UM_E_MEIO_LITRO_ORIGINAL", "REFRIGERANTE_UM_E_MEIO_LITRO_ZERO", "REFRIGERANTE_UM_LITRO_ORIGINAL", "REFRIGERANTE_UM_LITRO_ZERO", "REFRESCO", "SUCO", "CACHACA", "CERVEJA_GARRAFAO", "CERVEJA_GARRAFAPEQUENA", "CERVEJA_LATA", "CERVEJA_LATA_PEQUENA", "CERVEJA_LATAO","CERVEJA_LATA_ZERO", "CERVEJA_LONG", "CERVEJA_LONG_ZERO", "CHOPP", "DESTILADO", "ESPUMANTE", "VINHO", "VINHO_BRANCO", "VINHO_ROSE", "VINHO_TINTO", "VODKA", "WHISKY"),
         "MATINAIS", List.of("ACHOCOLATADO_EM_PO", "BEBIDALACTEA", "BOLACHAS", "CAFE_A_VACO", "CAFE_CAPSULA", "CAFE_GRAOS", "CAFE_SOLUVEL", "CAPPUCCINO", "CEREAL", "CHA", "CHOCOLATES_EM_BARRA", "DOCES_E_SOBREMESA", "ERVA_MATE", "GELEIAS_E_PASTAS", "IOGURTE", "LEITE", "LEITE_EM_PO", "LEITE_FERMENTADO", "TAPIOCA", "TORRADA"),
-        "HIGIENEPESSOAL", List.of("ABASORVENTE", "APARELHO_DEPILADOR", "CONDICIONADOR", "CREME_DENTAL", "CREMES", "DESODORANTE", "ESCOVA_CAPILAR", "ESCOVA_DENTAL", "ESPONJA_BANHO", "FRALDA", "HIGIENE_BOCAL", "PAPEL_HIGIENICO", "PRESERVATIVO", "SABONETE_BARRA", "SABONETE_LIQUIDO", "SHAMPOO", "TALCOS", "TOALHA_UMEDECIDA", "PRODUTOS_DIVERSOS_HIGIENE"),
+        "HIGIENE_PESSOAL", List.of("ABASORVENTE", "APARELHO_DEPILADOR", "CONDICIONADOR", "CREME_DENTAL", "CREMES", "DESODORANTE", "ESCOVA_CAPILAR", "ESCOVA_DENTAL", "ESPONJA_BANHO", "FRALDA", "HIGIENE_BOCAL", "PAPEL_HIGIENICO", "PRESERVATIVO", "SABONETE_BARRA", "SABONETE_LIQUIDO", "SHAMPOO", "TALCOS", "TOALHA_UMEDECIDA", "PRODUTOS_DIVERSOS_HIGIENE"),
         "LIMPEZA", List.of("AGUA_SANITARIA", "ALCOOL_GEL", "ALCOOL_LIQUIDO", "AMACIANTE", "DETERGENTE_EM_PO", "DETERGENTE_LIQUIDO", "DESENGORDURANTE", "DESINFETANTE", "DESODORIZADOR", "LAVAROUPAS_LIQUIDO", "LIMPADORES_DIVERSOS", "MULTIINSETICIDA", "MULTIUSO", "PANO", "PRODUTOS_CARRO", "REPELENTE", "SABAO_EM_BARRA", "SABAO_EM_PO", "SABAO_LIQUIDO", "SACO_LIXO", "UTENCILIOS_PARA_LIMPEZA", "PRODUTOS_DIVERSOS_LIMPEZA"),
         "PADARIA", List.of("BAURU_CALABREZA", "BAURU_FRANGORE_QUEIJAO", "BOLO", "BOMBA_SABORES", "COXINHA", "CUPCAKE", "ENROLADO_SALSICHA", "FERMENTO", "KIBE", "MINI_COXINHA", "MINI_ENROLADINHO_SALSICHA", "MINI_KIBE", "MINI_PASTEL", "MINI_SALGADOS_DIVERSOS", "MINI_CHURROS", "PAES_DIVERSOS", "PAO_BISNAGUINHA", "PAO_DE_FORMA", "PAO_DE_LEITE", "PAO_DE_QUEIJO", "PAO_FOLHADO", "PAO_HAMBURGUER", "PAO_HOTDOG", "PAO_INTEGRAL", "PAO_TORTILHA", "PASTEL", "RISOLES", "SONHO"),
         "HORTIFRUIT", List.of("REFRIGERADO", "TEMPERATURA_AMBIENTE"),
@@ -109,8 +109,8 @@ public class DataLoader implements ApplicationRunner{
             new Product(null, "Arroz Branco Kika 5kg", "38.94", null, null, "Un", categoryMap.get("ALIMENTOSBASICOS"), subcategoryMap.get("ARROZ"), null),
             
             new Product(null, "Azeite Terrano Extra Virgem 500ml", "55,64", null, null, "Un", categoryMap.get("ALIMENTOSBASICOS"), subcategoryMap.get("AZEITE"), null),
-            new Product(null, "Azeite de Oliva Tipo Único Português Dia a Dia Gallo 500ml", "66.21", null, null, "Un", categoryMap.get("ALIMENTOSBASICOS"), subcategoryMap.get("AZEITE"), null),
-            new Product(null, "Azeite Andorinha 500 ml", "66.77", null, null, "Un", categoryMap.get("ALIMENTOSBASICOS"), subcategoryMap.get("AZEITE"), null),
+            new Product(null, "Azeite de Oliva Tipo Unico Gallo 500ml", "66.21", null, null, "Un", categoryMap.get("ALIMENTOSBASICOS"), subcategoryMap.get("AZEITE"), null),
+            new Product(null, "Azeite Andorinha 500ml", "66.77", null, null, "Un", categoryMap.get("ALIMENTOSBASICOS"), subcategoryMap.get("AZEITE"), null),
             new Product(null, "Azeite Dende Cepera 100ml", "15.42", null, null, "Un", categoryMap.get("ALIMENTOSBASICOS"), subcategoryMap.get("AZEITE"), null),
             new Product(null, "Azeite de Dendê Hemmer Vidro 200ml", "24.35", null, null, "Un", categoryMap.get("ALIMENTOSBASICOS"), subcategoryMap.get("AZEITE"), null),
             new Product(null, "Óleo de Soja Coamo 900ml", "7,02", null, null, "Un", categoryMap.get("ALIMENTOSBASICOS"), subcategoryMap.get("AZEITE"), null),
@@ -122,17 +122,17 @@ public class DataLoader implements ApplicationRunner{
             new Product(null, "Farinha de Arroz Urbano 1kg", "8.14", null, null, "Un", categoryMap.get("ALIMENTOSBASICOS"), subcategoryMap.get("FARINHA"), null),
             new Product(null, "Farinha de Rosca Marsala 500g", "10.45", null, null, "Un", categoryMap.get("ALIMENTOSBASICOS"), subcategoryMap.get("FARINHA"), null),
             
-            new Product(null, "Farofa de Mandioca Tradicional Yoki 250g", "6.56", null, null, "Un", categoryMap.get("ALIMENTOSBASICOS"), subcategoryMap.get("FAROFA"), null),
-            new Product(null, "Farofa Yoki Tradicional Temperada 400g", "10.59", null, null, "Un", categoryMap.get("ALIMENTOSBASICOS"), subcategoryMap.get("FAROFA"), null),
+            new Product(null, "Farofa de Mandioca Tradicional Yoki 400g", "10.59", null, null, "Un", categoryMap.get("ALIMENTOSBASICOS"), subcategoryMap.get("FAROFA"), null),
             new Product(null, "Farofa de Soja Pinduca Embalagem 250g", "5.13", null, null, "Un", categoryMap.get("ALIMENTOSBASICOS"), subcategoryMap.get("FAROFA"), null),
             new Product(null, "Farofa de Milho Pinduca 400g", "7.23", null, null, "Un", categoryMap.get("ALIMENTOSBASICOS"), subcategoryMap.get("FAROFA"), null),
             new Product(null, "Farofa Kanty Suave 250g", "5.51", null, null, "Un", categoryMap.get("ALIMENTOSBASICOS"), subcategoryMap.get("FAROFA"), null),
+            new Product(null, "Farofa Kanty Calabresa 250g", "5.51", null, null, "Un", categoryMap.get("ALIMENTOSBASICOS"), subcategoryMap.get("FAROFA"), null),
+            new Product(null, "Farofa Kanty Picanha 250g", "5.51", null, null, "Un", categoryMap.get("ALIMENTOSBASICOS"), subcategoryMap.get("FAROFA"), null),
             
             new Product(null, "Feijão Preto Rico Caldo 1kg", "7.67", null, null, "Un", categoryMap.get("ALIMENTOSBASICOS"), subcategoryMap.get("FEIJAO"), null),
             new Product(null, "Feijão Carioca Rico Caldo 1kg", "10.03", null, null, "Un", categoryMap.get("ALIMENTOSBASICOS"), subcategoryMap.get("FEIJAO"), null),
             new Product(null, "Feijão Vermelho Rico Caldo 1kg", "14.79", null, null, "Un", categoryMap.get("ALIMENTOSBASICOS"), subcategoryMap.get("FEIJAO"), null),
-            new Product(null, "Feijão Preto São Francisco 1kg", "8.56", null, null, "Un", categoryMap.get("ALIMENTOSBASICOS"), subcategoryMap.get("FEIJAO"), null),
-            new Product(null, "Feijão Branco Kanty 500g", "10.34", null, null, "Un", categoryMap.get("ALIMENTOSBASICOS"), subcategoryMap.get("FEIJAO"), null),
+            new Product(null, "Feijão Branco Sinhá 500g", "13.70", null, null, "Un", categoryMap.get("ALIMENTOSBASICOS"), subcategoryMap.get("FEIJAO"), null),
             
             new Product(null, "Milho para Pipoca Kanty 400g", "4.04", null, null, "Un", categoryMap.get("ALIMENTOSBASICOS"), subcategoryMap.get("GRAOS"), null),
             new Product(null, "Canjica Branca Kanty 400g", "6.56", null, null, "Un", categoryMap.get("ALIMENTOSBASICOS"), subcategoryMap.get("GRAOS"), null),
